0ee5c444140a2038e44bf08eea04040929f60c97
atobad <atobad@gmail.com>
2017-03-27 23:08:38 +0800

    Pull from Github and edit by Visual Studio 2017 Community to test gitignore and compatibility.

--- sam/HelloWorld/HelloWorld/Form1.Designer.cs
+++ sam/HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnShowTaiwan = new System.Windows.Forms.Button();
+            this.Australia = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnShowTaiwan
@@ -40,11 +41,21 @@
             this.btnShowTaiwan.Text = "Taiwan";
             this.btnShowTaiwan.UseVisualStyleBackColor = true;
             // 
+            // Australia
+            // 
+            this.Australia.Location = new System.Drawing.Point(12, 50);
+            this.Australia.Name = "Australia";
+            this.Australia.Size = new System.Drawing.Size(75, 23);
+            this.Australia.TabIndex = 1;
+            this.Australia.Text = "button1";
+            this.Australia.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 343);
+            this.Controls.Add(this.Australia);
             this.Controls.Add(this.btnShowTaiwan);
             this.Name = "Form1";
             this.Text = "What a wonderful world.....";
@@ -55,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnShowTaiwan;
+        private System.Windows.Forms.Button Australia;
     }
 }
 
